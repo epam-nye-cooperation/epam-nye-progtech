5bdb537ed64a911a34b8f5965eaf0a6ec1eb64c7
Csaba Lamfalusi <csaba_lamfalusi@epam.com>
2021-11-06 07:25:31 +0100

    Progress of week 9

---
+++ lessons/week_9/sudoku/src/main/java/hu/nye/progtech/sudoku/configuration/CommandConfiguration.java
@@ -0,0 +1,48 @@
+package hu.nye.progtech.sudoku.configuration;
+
+import java.util.List;
+
+import hu.nye.progtech.sudoku.model.GameState;
+import hu.nye.progtech.sudoku.service.command.Command;
+import hu.nye.progtech.sudoku.service.command.InputHandler;
+import hu.nye.progtech.sudoku.service.command.impl.DefaultCommand;
+import hu.nye.progtech.sudoku.service.command.impl.ExitCommand;
+import hu.nye.progtech.sudoku.service.command.impl.PrintCommand;
+import hu.nye.progtech.sudoku.service.command.impl.PutCommand;
+import hu.nye.progtech.sudoku.service.command.performer.PutPerformer;
+import hu.nye.progtech.sudoku.service.validator.impl.MapValidatorComposer;
+import hu.nye.progtech.sudoku.ui.MapPrinter;
+import hu.nye.progtech.sudoku.ui.PrintWrapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CommandConfiguration {
+
+    @Bean
+    public InputHandler inputHandler(List<Command> commandList) {
+        return new InputHandler(commandList);
+    }
+
+    @Bean
+    public PrintCommand printCommand(GameState gameState, MapPrinter mapPrinter) {
+        return new PrintCommand(gameState, mapPrinter);
+    }
+
+    @Bean
+    public PutCommand putCommand(GameState gameState, PutPerformer putPerformer, MapValidatorComposer mapValidatorComposer,
+                                 MapPrinter mapPrinter, PrintWrapper printWrapper) {
+        return new PutCommand(gameState, putPerformer, mapValidatorComposer, mapPrinter, printWrapper);
+    }
+
+    @Bean
+    public ExitCommand exitCommand(GameState gameState) {
+        return new ExitCommand(gameState);
+    }
+
+    @Bean
+    public DefaultCommand defaultCommand(PrintWrapper printWrapper) {
+        return new DefaultCommand(printWrapper);
+    }
+
+}
